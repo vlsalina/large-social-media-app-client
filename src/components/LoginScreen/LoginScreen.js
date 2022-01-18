9dd7d4657a722bf3f5a2be07b549b3ff544f2e3f
vlsalina <vlsalina@ucsd.edu>
2022-01-18 00:18:50 -0800

    Reformatted class names to use BEM naming convention in Login screen.

--- src/components/LoginScreen/LoginScreen.js
+++ src/components/LoginScreen/LoginScreen.js
@@ -26,21 +26,21 @@ const LoginScreen = () => {
   });
 
   return (
-    <div className="loginScreen">
+    <div className="login">
       {loading ? (
         <Loader />
       ) : (
         <>
           <LoginForm setLoading={setLoading} />
-          <div className="loginScreen-wrapper">
-            <div className="loginScreen-wrapper-header">
+          <div className="login--box-1">
+            <div className="login--box-2">
               <h1>Welcome back.</h1>
             </div>
             <ul>
               {networks.slice(0, 4).map((x) => (
                 <li key={x.network}>
                   <a href={x.url} target="_blank">
-                    <button type="button" className="loginScreen-media-button">
+                    <button type="button" className="login__button">
                       Sign in with {x.network}
                     </button>
                   </a>
@@ -49,17 +49,17 @@ const LoginScreen = () => {
               <li key={networks[4].network}>
                 <button
                   type="button"
-                  className="loginScreen-media-button"
+                  className="login__button"
                   onClick={clickHandler}
                 >
                   Sign in with {networks[4].network}
                 </button>
               </li>
             </ul>
-            <div className="loginScreen-toRegister">
+            <div className="login--box-3">
               <p>
                 No account?{" "}
-                <Link className="toRegister-red" to="/register">
+                <Link className="create" to="/register">
                   <b>Create One</b>
                 </Link>
               </p>
