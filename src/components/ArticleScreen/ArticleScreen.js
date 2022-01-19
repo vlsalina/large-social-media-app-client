8c4e432dbd1246f0ccba12987345797cd4e704e8
vlsalina <vlsalina@ucsd.edu>
2022-01-18 16:59:51 -0800

    Implemented Follow author functionality.

--- src/components/ArticleScreen/ArticleScreen.js
+++ src/components/ArticleScreen/ArticleScreen.js
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { useParams } from "react-router-dom";
+import { Context } from "../../App";
 import Header from "../Header/Header";
 import { useSelector } from "react-redux";
 import "./ArticleScreen.css";
@@ -10,6 +11,7 @@ import { TiMessages } from "react-icons/ti";
 import { IconContext } from "react-icons";
 import Footer from "../Footer/Footer";
 import SocialMenu from "../SocialMenu/SocialMenu";
+import axios from "axios";
 
 const styles = {
   icons: {
@@ -18,7 +20,10 @@ const styles = {
 };
 
 const ArticleScreen = () => {
+  const { domain } = useContext(Context);
   const [article, setArticle] = useState();
+  const [following, setFollowing] = useState(false);
+  const user = useSelector((state) => state.user);
   const { articleId } = useParams();
   const articles = useSelector((state) => state.articles);
 
@@ -32,6 +37,34 @@ const ArticleScreen = () => {
       });
     setArticle(result);
   }, []);
+
+  useEffect(() => {
+    axios
+      .get(`${domain}/api/users/getUser?id=${user._id}`, {
+        headers: { authorization: `Bearer ${user.accessToken}` },
+      })
+      .then((response) => {
+        if (response.data.following.includes(article.authorId)) {
+          setFollowing(true);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  const followHandler = async () => {
+    try {
+      let { data } = await axios.patch(
+        `${domain}/api/users/${following ? "unfollow" : "follow"}`,
+        { userId: article.authorId },
+        { headers: { authorization: `Bearer ${user.accessToken}` } }
+      );
+      setFollowing(!following);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <main className="article">
@@ -71,8 +104,9 @@ const ArticleScreen = () => {
                 <button
                   className="article__button--1 article--padding-1"
                   type="button"
+                  onClick={followHandler}
                 >
-                  Follow
+                  {following ? "Unfollow" : "Follow"}
                 </button>
               </div>
               <div className="article--box-8">
@@ -108,22 +142,26 @@ const ArticleScreen = () => {
       </article>
       <div className="article--box-5">
         <div className="article--box-9">
-          <div className="article--box-10">
-            <button type="button" className="article__button--2">
-              <IconContext.Provider value={styles.icons}>
-                <AiOutlineLike />
-                &nbsp; &nbsp;{article.likes.length}
-              </IconContext.Provider>
-            </button>
-          </div>
-          <div className="article--box-10">
-            <button type="button" className="article__button--2">
-              <IconContext.Provider value={styles.icons}>
-                <AiOutlineMessage />
-                &nbsp; &nbsp;{article.replies.length}
-              </IconContext.Provider>
-            </button>
-          </div>
+          {article && (
+            <div className="article--box-10">
+              <button type="button" className="article__button--2">
+                <IconContext.Provider value={styles.icons}>
+                  <AiOutlineLike />
+                  &nbsp; &nbsp;{article.likes.length}
+                </IconContext.Provider>
+              </button>
+            </div>
+          )}
+          {article && (
+            <div className="article--box-10">
+              <button type="button" className="article__button--2">
+                <IconContext.Provider value={styles.icons}>
+                  <TiMessages />
+                  &nbsp; &nbsp;{article.replies.length}
+                </IconContext.Provider>
+              </button>
+            </div>
+          )}
         </div>
         <SocialMenu />
       </div>
