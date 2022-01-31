68137b620a3099902d9baa77f8c4b22bec6aa89f
vlsalina <vlsalina@ucsd.edu>
2022-01-31 01:43:06 -0800

    Implemented notification for user when Profile screen has empty tabs.

--- src/components/ProfileScreen/ProfileScreen.js
+++ src/components/ProfileScreen/ProfileScreen.js
@@ -239,7 +239,14 @@ const ProfileScreen = () => {
               >
                 <div className="profilescreen--box-2">
                   <div>
-                    <h2>No articles written by this author. </h2>
+                    {flag === "articles" && <h2>No articles written.</h2>}
+                    {flag === "favorites" && <h2>No articles bookmarked.</h2>}
+                    {flag === "notifications" && (
+                      <h2>No replies to any of your articles (yet).</h2>
+                    )}
+                    {flag === "following" && (
+                      <h2>No authors being followed.</h2>
+                    )}
                   </div>
                 </div>
               </Wrapper>
