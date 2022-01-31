2bf37cb4c5dfbc6f2b28854692aed1dba6a163d2
vlsalina <vlsalina@ucsd.edu>
2022-01-30 16:13:14 -0800

    Implemented add follow button to Profile screen.

--- src/components/ProfileScreen/ProfileScreen.js
+++ src/components/ProfileScreen/ProfileScreen.js
@@ -14,6 +14,7 @@ import { Link } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 import Avatar from "../Avatar/Avatar";
 import { breadcrumbs } from "../../data/data";
+import AuthorProfile from "../AuthorProfile/AuthorProfile";
 
 const FadeIn = ({ children }) => {
   const [isVisib, setIsVisib] = React.useState(false);
@@ -44,32 +45,6 @@ const Breadcrumbs = ({ getData }) => {
         ))}
       </ul>
     </div>
-  );
-};
-
-const AuthorProfile = ({ author }) => {
-  return (
-    <aside className="authorprofile">
-      <div className="authorprofile__avatar">
-        <img src={`/assets/large-logo.png`} />
-      </div>
-      <div className="authorprofile__author">
-        <p>
-          {author.firstname} {author.lastname}
-        </p>
-      </div>
-      <div className="authorprofile__story">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </p>
-      </div>
-    </aside>
   );
 };
 
